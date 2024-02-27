50e15b7e86a7cbbf3d19dc6fdd8059a79d2dae9b
Nikita Klochko <nikita.klochko@binerals.com>
2024-02-27 23:22:25 +0200

    removed submodule and added folder

---
+++ backend/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(3000);
+  console.log('env vars:', process.env.environment, process.env.test_var, 123);
+}
+bootstrap();
